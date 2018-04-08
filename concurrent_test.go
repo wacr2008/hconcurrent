85add56b87155d41440feb47c00362ba4a34b5b7
haiswork <whaibin01@126.com>
2018-04-08 08:29:17 +0800

    update test case

--- concurrent_test.go
+++ concurrent_test.go
@@ -10,30 +10,28 @@ func TestConcurrent(t *testing.T) {
 	doFuncCount := 4
 	listCount := 3
 	inputs := []int{1, 2, 3, 4, 5}
-	list := []interface{}{}
-	for i := 0; i < listCount; i++ {
-		list = append(list, inputChanSize, doFuncCount,
-			func(i interface{}) (interface{}, error) {
-				n := i.(int)
-				return n + 1, nil
-			})
-	}
 	m := map[int]int{}
 	l := new(sync.Mutex)
-	list = append(list, inputChanSize, doFuncCount,
-		func(i interface{}) (interface{}, error) {
+
+	c := NewConcurrent2(
+		inputChanSize, doFuncCount, testDo,
+		inputChanSize, doFuncCount, testDo,
+		inputChanSize, doFuncCount, testDo,
+		inputChanSize, doFuncCount, func(i interface{}) (interface{}, error) {
 			l.Lock()
 			defer l.Unlock()
 			n := i.(int)
 			m[n] = n
 			return n, nil
-		})
-	c := NewConcurrent2(list...)
+		},
+	)
+
 	c.Do()
 	for i := 0; i < len(inputs); i++ {
 		c.Input(inputs[i])
 	}
 	c.Stop()
+
 	for i := 0; i < len(inputs); i++ {
 		n := inputs[i] + listCount
 		if m[n] != n {
